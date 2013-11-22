7e174b3de152ed39356ecea59bc0d7b9e469e933
Eric <Eric@Eric-PC>
2013-11-23 03:05:33 +0800

    update

--- apps/topic/templatetags/template_tags.py
+++ apps/topic/templatetags/template_tags.py
@@ -3,9 +3,9 @@
 from apps.accounts.function import gfm
 from apps.people.models import Notice, Message
 from django import template
-from django.template.defaultfilters import stringfilter
+from django.template.defaultfilters import stringfilter, timesince as _timesince
+from django.template.defaultfilters import date as _date
 from django.utils.safestring import mark_safe
-from django.template.defaultfilters import timesince as _timesince
 from django.utils.timezone import LocalTimezone
 import datetime
 import logging
@@ -18,40 +18,40 @@ register = template.Library()
 def hello(value,msg="Hello"):   
     return "%s,%s！" % (msg,value)   
 
-# @register.filter
-# def time_since(d, now=None):
-#     # Convert datetime.date to datetime.datetime for comparison.
-#     if not d:
-#         return ''
-#     if not isinstance(d, datetime.datetime):
-#         d = datetime.datetime(d.year, d.month, d.day)
-#     if now and not isinstance(now, datetime.datetime):
-#         now = datetime.datetime(now.year, now.month, now.day)
-#     if not now:
-#         if d.tzinfo:
-#             now = datetime.datetime.now(LocalTimezone(d))
-#         else:
-#             now = datetime.datetime.now()
-#     # ignore microsecond part of 'd' since we removed it from 'now'
-#     delta = now - (d - datetime.timedelta(0, 0, d.microsecond))
-#     since = delta.days * 24 * 60 * 60 + delta.seconds
-#     if since // (60 * 60 * 24) < 3:
-#         return _("%s ago") % _timesince(d)
-#     return _date(d, "Y-m-d H:i")
-
-
 @register.filter
-def time_since(value):
-    #logging.error(value)
-    now = datetime.datetime.now()
-    try:
-        difference = now - value
-    except:
-        return value
-    if difference <= datetime.timedelta(minutes=1):
-        return '刚刚'
-    #logging.error(timesince(value)+'   '+timesince(value).split(', ')[0])
-    return _timesince(value).split(', ')[0]+'前'
+def time_since(d, now=None):
+    # Convert datetime.date to datetime.datetime for comparison.
+    if not d:
+        return ''
+    if not isinstance(d, datetime.datetime):
+        d = datetime.datetime(d.year, d.month, d.day)
+    if now and not isinstance(now, datetime.datetime):
+        now = datetime.datetime(now.year, now.month, now.day)
+    if not now:
+        if d.tzinfo:
+            now = datetime.datetime.now(LocalTimezone(d))
+        else:
+            now = datetime.datetime.now()
+    # ignore microsecond part of 'd' since we removed it from 'now'
+    delta = now - (d - datetime.timedelta(0, 0, d.microsecond))
+    since = delta.days * 24 * 60 * 60 + delta.seconds
+    if since // (60 * 60 * 24) < 3:
+        return "%s前" % _timesince(d)
+    return _date(d, "Y-m-d H:i")
+
+
+# @register.filter
+# def time_since(value):
+#     #logging.error(value)
+#     now = datetime.datetime.now()
+#     try:
+#         difference = now - value
+#     except:
+#         return value
+#     if difference <= datetime.timedelta(minutes=1):
+#         return '刚刚'
+#     #logging.error(timesince(value)+'   '+timesince(value).split(', ')[0])
+#     return _timesince(value).split(', ')[0]+'前'
 
 @register.filter
 def adjust_link(value):
